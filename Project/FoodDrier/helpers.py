f3c1ed2bb6c2a9668009da20a345cc21e8316e75
Kwenda_Supreme <57506993+Kwenda-supreme@users.noreply.github.com>
2020-12-06 14:15:32 -0100

    Add files via upload

--- Project/FoodDrier/helpers.py
+++ Project/FoodDrier/helpers.py
@@ -8,6 +8,319 @@ ScreenManager:
     DryingWindow:
     Drying:
     ResumeDrying:
+    History:
+    Done:
+    
+<MenuScreen>:
+    name: 'menu'
+    
+                      
+            
+    MDFloatingActionButton:
+        icon: "Images/white.png"
+        size_hint: 1, 0.9
+        #pos_hint: {"center_x": 0.52}       
+   
+         
+    MDFloatingActionButton:
+        icon: "Images/btnpower.png"
+        elevation_normal: 12
+        size_hint: .4, .3
+        pos_hint: {"center_x": 0.5, "center_y": 0.45}
+        elevation_normal: 30
+        on_release:
+            app.popup()
+            root.manager.current = 'power on'
+            root.manager.transition.direction = 'left'
+            # app.power_on()
+            app.err_handling()
+            
+    MDFloatingActionButtonSpeedDial:
+        data: app.data
+        rotation_root_button: False
+         
+        
+    NavigationLayout:
+
+        ScreenManager:
+            Screen:
+
+                BoxLayout:
+                    orientation: 'vertical'
+                    pos_hint: {"center_x": 0.5, "center_y": 1.4}
+                    
+                    
+
+                    MDToolbar:
+                        title: "Food Drier Off"
+                        elevation: 10
+                        left_action_items: [['menu', lambda x: nav_drawer.toggle_nav_drawer()]]
+                        md_bg_color: .752,.514,.18,1
+
+                    #Widget:
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            BoxLayout:
+                orientation: 'vertical'
+                spacing: '8dp'
+                padding: '8dp'
+                
+                Image:
+                    source: 'Images/food.jpg'
+                
+                ScrollView:
+                    MDList:
+                        OneLineIconListItem:
+                            text: 'History'
+                            
+                            on_release:
+                                root.manager.current = 'history'
+                                root.manager.transition.direction = 'left'
+                            
+                            IconLeftWidget:
+                                icon: 'poll'
+                        OneLineIconListItem:
+                            text: 'About Us'
+                            IconLeftWidget:
+                                icon: 'account-group'
+                            
+                        OneLineIconListItem:
+                            text: 'Help'
+                            IconLeftWidget:
+                                icon: 'help-circle-outline'
+                    
+       
+
+
+<HomeWithPowerOn>:
+
+    name: 'power on'
+    
+    BoxLayout:
+        orientation: 'vertical'
+        pos_hint: {"center_x": 0.5, "center_y": 1.4}
+        color: "black"
+         
+        MDToolbar:
+            md_bg_color: .752,.514,.18,1
+            title: 'On'  
+            
+    MDFloatingActionButton:
+        icon: "Images/white.png"
+        size_hint: 1, 0.9
+        #pos_hint: {"center_x": 0.52} 
+        
+    MDRaisedButton:
+        text: "Restart"
+        text_color: 0, 0, 0, 1
+        pos_hint: {"x" : 0.7, "y" : 0.7}
+        md_bg_color: .752,.514,.18,1
+        font_weight: "bold"
+        on_press: 
+            root.manager.current = 'menu'              
+        
+    MDFloatingActionButton:
+        icon: "Images/poweroff2.png"
+        size_hint: .4, .3
+        pos_hint: {"center_x": 0.5, "center_y": 0.45}
+        user_font_size: "25sp"
+        elevation_normal: 30
+        on_release:
+            #app.power_off()
+            #app.back_on_select()
+            # app.dry_off()
+            root.manager.current = 'menu' 
+            root.manager.transition.direction = 'right' 
+
+    
+                    
+    MDRaisedButton:
+        text: "DRY"
+        text_color: 0, 0, 0, 1
+        pos_hint: {"center_x" : 0.5, "y" : 0.1}
+        md_bg_color: .752,.514,.18,1
+        font_weight: "bold"
+        on_press: 
+            root.manager.current = 'select'
+            #app.dry()        
+
+<SelectItemWindow>:
+    name: 'select'
+
+    BoxLayout:
+        orientation: 'vertical'
+        pos_hint: {"center_x": 0.5, "center_y": 1.4}
+        #color: "8,.898,.1,1"
+     
+        MDToolbar:
+            title: 'Select Food'           
+            md_bg_color: .752, .514, .18, 1
+            
+            MDRaisedButton:
+                text: "Restart"
+                text_color: 0, 0, 0, 1
+                pos_hint: {"x" : 0.1}
+                #md_bg_color: .752,.514,.18,1
+                font_weight: "bold"
+                on_press: 
+                    root.manager.current = 'menu'
+                    
+                
+            
+   
+    MDFloatingActionButton:
+        icon: "Images/white.png"
+        size_hint: 1, 0.9
+       
+    # FRUITS
+    MDFloatingActionButton:
+        id: zipatso
+        icon: "Images/apppl.jpg"
+        pos_hint: {"x": .1, "y": .51}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'down'
+            #app.fruits()
+        size_hint: .35, .3
+        md_bg_color: .952,.952,.952, 0
+        #user_font_size: "150sp"
+        
+    MDRaisedButton:
+        text: 'Fruits'
+        pos_hint: {'x':.1,'center_y':0.48}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'down'
+            #app.fruits()
+        text_color: 0, 0, 0, 1
+        md_bg_color: 1, 1, 1, 1
+        font_weight: "bold"
+        size_hint: .35, .06
+        
+    # MEAT
+    MDFloatingActionButton:
+        icon: "Images/meat.jpg"
+        pos_hint: {"x": .52, "y": .51}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'up'
+            #app.nyama()
+        size_hint: .35, .3
+        md_bg_color: .952,.952,.952, 0
+        #user_font_size: "150sp"
+    
+    MDFlatButton:
+        text: 'Meat'
+        pos_hint: {'x':0.52,'center_y':0.48}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'up'
+            #app.nyama()
+        text_color: 0, 0, 0, 1
+        md_bg_color: 1, 1, 1, 1
+        font_weight: "bold"
+        size_hint: .35, .06
+        
+    # VEGIES
+    MDFloatingActionButton:
+        icon: "Images/veges.jpg"
+        pos_hint: {"x": .1, "y": .1}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'up'
+            #app.vegies()
+        size_hint: .35, .3
+        md_bg_color: .952,.952,.952, 0
+        #user_font_size: "150sp"
+    
+    MDFlatButton:
+        text: 'Vegies'
+        pos_hint: {'x':0.1,'y':0.04}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'up'
+            #app.vegies()
+        text_color: 0, 0, 0, 1
+        md_bg_color: 1, 1, 1, 1
+        font_weight: "bold"
+        size_hint: .35, .06
+        
+    # FISH
+    MDFloatingActionButton:
+        icon: "Images/fish.png"
+        pos_hint: {"x": .52, "y": .1}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'down'
+            #app.fish()
+        size_hint: .35, .3
+        md_bg_color: .952,.952,.952, 0
+        #user_font_size: "150sp"
+    
+    MDFlatButton:
+        text: 'Fish'
+        pos_hint: {'x':0.52,'y':0.04}
+        on_press: 
+            root.manager.current = 'dry'
+            root.manager.transition.direction = 'down'
+            #app.fish()
+        text_color: 0, 0, 0, 1
+        md_bg_color: 1, 1, 1, 1
+        font_weight: "bold"
+        size_hint: .35, .06
+        
+<DryingWindow>:
+    name: "dry"
+    
+    BoxLayout:
+        orientation: 'vertical'
+        pos_hint: {"center_x": 0.5, "center_y": 1.4}
+        color: "black"
+    
+        
+     
+        MDToolbar:
+            title: 'Dry'
+            md_bg_color: .752,.514,.18,1
+            
+    MDFloatingActionButton:
+        icon: "Images/white.png"
+        size_hint: 1, 0.9
+        
+    MDRaisedButton:
+        text: "Restart"
+        text_color: 0, 0, 0, 1
+        pos_hint: {"x" : 0.7, "y" : 0.7}
+        md_bg_color: .752,.514,.18,1
+        font_weight: "bold"
+        on_press: 
+            root.manager.current = 'menu'
+
+    MDRaisedButton:
+        text: 'START'
+        pos_hint: {'center_x':.5,'center_y':0.5}
+        on_press: 
+            root.manager.current = 'drying'
+            root.manager.transition.direction = 'left'
+            #app.start_stop()
+
+        text_color: 0, 0, 0, 1
+        md_bg_color: .752,.514,.18,1
+        font_weight: "bold"
+
+    MDRaisedButton:
+        text: 'BACK'
+        pos_hint: {'center_x':.5,'center_y':0.3}
+        on_press: 
+            root.manager.current = 'select'
+            root.manager.transition.direction = 'right'
+            #app.back_on_select()
+        text_color: 0, 0, 0, 1
+        md_bg_color: .752,.514,.18,1
+        font_weight: "bold"
+
   
 <ResumeDrying>:
     name : "resume"
@@ -21,21 +334,30 @@ ScreenManager:
      
         MDToolbar:
             title: 'Resume'
-           
-            left_action_items: [["menu", lambda x: app.navigation_draw()]]
+            md_bg_color: .752,.514,.18,1
+            
+    MDFloatingActionButton:
+        icon: "Images/white.png"
+        size_hint: 1, 0.9
 
-    MDRectangleFlatButton:
+    MDRaisedButton:
         text: 'RESUME'
         pos_hint: {'center_x':.5,'center_y':0.5}
-        on_press: root.manager.current = 'drying'
+        on_press: 
+            root.manager.current = 'drying'
+            root.manager.transition.direction = 'down'
+            #app.start_stop()
         text_color: 0, 0, 0, 1
         md_bg_color: .752,.514,.18,1
         font_weight: "bold"
 
-    MDRectangleFlatButton:
+    MDRaisedButton:
         text: 'BACK'
         pos_hint: {'center_x':.5,'center_y':0.3}
-        on_press: root.manager.current = 'select'
+        on_press: 
+            root.manager.current = 'select'
+            root.manager.transition.direction = 'right'
+            #app.back_on_select()
         text_color: 0, 0, 0, 1
         md_bg_color: .752,.514,.18,1
         font_weight: "bold"
@@ -48,209 +370,56 @@ ScreenManager:
         orientation: 'vertical'
         pos_hint: {"center_x": 0.5, "center_y": 1.4}
         color: "black"
-    
-        
-     
+         
         MDToolbar:
             title: 'Drying'
-           
-            # left_action_items: [["menu", lambda x: app.navigation_draw()]]
+            md_bg_color: .752,.514,.18,1
+            
+    MDFloatingActionButton:
+        icon: "Images/white.png"
+        size_hint: 1, 0.9
 
-    MDRectangleFlatButton:
+    MDRaisedButton:
         text: 'STOP'
         pos_hint: {'center_x':.5,'center_y':0.5}
-        on_press: root.manager.current = 'resume'
+        on_press: 
+            root.manager.current = 'resume'
+            root.manager.transition.direction = 'up'
+            #app.start_stop_stop()
         text_color: 0, 0, 0, 1
         md_bg_color: .752,.514,.18,1
         font_weight: "bold"
 
-    MDRectangleFlatButton:
+    MDRaisedButton:
         text: 'BACK'
         pos_hint: {'center_x':.5,'center_y':0.3}
         text_color: 0, 0, 0, 1
         md_bg_color: .76,.73,.73,1
         font_weight: "bold"
+    
+<History>
 
-<MenuScreen>:
-    name: 'menu'
+    name: "history"
     
-    BoxLayout:
-        orientation: 'vertical'
-        pos_hint: {"center_x": 0.5, "center_y": 1.4}
-    
-        MDToolbar:
-            title: 'Food Drier Off'
-           
-            # left_action_items: [["menu", lambda x: app.navigation_draw()]]
+    text: 'Hello' 
         
-          
-           
-    MDFlatButton:
-        text: "Power On"
-        text_color: 0, 0, 0, 1
-        md_bg_color: .752,.514,.18,1
-        text_size: 8
-        pos_hint: {"center_x": 0.5, "center_y": 0.65}
-        user_font_size: "25sp"
-        on_release: root.manager.current = 'power on'
-                 
-    # MDIconButton:
-    #     icon: "power"
-    #     pos_hint: {"center_x": 0.5, "center_y": 0.65}
-    #     user_font_size: "150sp"
-    #     on_release: root.manager.current = 'power on'
-
-    MDFlatButton:
-        text: "Help"
-        text_color: 1, 1, 1, 1
-        md_bg_color: .76,.73,.73,1
-        text_size: 8
-        pos_hint: {"center_x": .9, "center_y": 0.05}
-        user_font_size: "25sp"
-    MDFlatButton:
-        text: "HISTORY"
-        text_color: 0, 0, 0, 1
-        pos_hint: {"center_x" : 0.5, "center_y" : 0.35}
-        md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-    MDFlatButton:
-        text: "About Us"
-        text_color: 1, 1, 1, 1
-        md_bg_color: .76,.73,.73,1
-        text_size: 8
-        pos_hint: {"center_x" : .09, "center_y" : .05}
-
-
-
-<HomeWithPowerOn>:
-
-    name: 'power on'
-    
-    BoxLayout:
-        orientation: 'vertical'
-        pos_hint: {"center_x": 0.5, "center_y": 1.4}
-        color: "black"
-    
-        
-     
-        MDToolbar:
-            title: 'On'
-           
-            # left_action_items: [["menu", lambda x: app.navigation_draw()]]
-    
-    # MDIconButton:
-    #     icon: "on"
-    #     pos_hint: {"center_x": 0.5, "center_y": 0.65}
-    #     user_font_size: "150sp"
-    #     on_release: root.manager.current = 'menu'
-        
-    MDFlatButton:
-        text: "Turn Off "
-        text_color: 0, 0, 0, 1
-        md_bg_color: .752,.514,.18,1
-        text_size: 8
-        pos_hint: {"center_x": 0.5, "center_y": 0.65}
-        user_font_size: "25sp"
-        on_release:
-            root.manager.current = 'menu' 
-            root.manager.transition = "left"  
-
-    MDRectangleFlatButton:
-        text: "DRY"
-        text_color: 0, 0, 0, 1
-        pos_hint: {"center_x" : 0.5, "center_y" : 0.35}
-        md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-        on_press: root.manager.current = 'select'
-
-<SelectItemWindow>:
-    name: 'select'
-
-    BoxLayout:
-        orientation: 'vertical'
-        pos_hint: {"center_x": 0.5, "center_y": 1.4}
-        color: "black"
-     
-        MDToolbar:
-            title: 'Select Food'
-           
-            # left_action_items: [["menu", lambda x: app.navigation_draw()]]
-   
-
-    MDRectangleFlatButton:
-        text: 'FRUITS'
-        pos_hint: {'center_x':1/3,'center_y':0.7}
-        on_press: root.manager.current = 'dry'
+    MDRaisedButton:
+        text: '<'
+        pos_hint: {'x': .01, 'y': .01}
+        on_press: root.manager.current = 'menu'
         text_color: 0, 0, 0, 1
         md_bg_color: .752,.514,.18,1
         font_weight: "bold"
-
-    MDRectangleFlatButton:
-        text: 'VEGIES'
-        pos_hint: {'center_x':.7,'center_y':0.7}
-        on_press: root.manager.current = 'dry'
+       
+    MDRaisedButton:
+        text: 'CLEAR HISTORY'
+        pos_hint: {'x': .53, 'y': .01}
+        on_press: root.manager.current = 'history'
         text_color: 0, 0, 0, 1
         md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-
-    MDRectangleFlatButton:
-        text: 'MEAT'
-        pos_hint: {'center_x':1/3,'center_y':0.5}
-        on_press: root.manager.current = 'dry'
-        text_color: 0, 0, 0, 1
-        md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-
-    MDRectangleFlatButton:
-        text: 'FISH'
-        pos_hint: {'center_x':.7,'center_y':0.5}
-        on_press: root.manager.current = 'dry'
-        text_color: 0, 0, 0, 1
-        md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-
-    MDRectangleFlatButton:
-        text: 'BACK'
-        pos_hint: {'center_x':.5,'center_y':0.3}
-        on_press: root.manager.current = 'power on'
-        text_color: 0, 0, 0, 1
-        md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-
-<DryingWindow>:
-    name: "dry"
-    
-    BoxLayout:
-        orientation: 'vertical'
-        pos_hint: {"center_x": 0.5, "center_y": 1.4}
-        color: "black"
-    
-        
-     
-        MDToolbar:
-            title: 'Dry'
-           
-            # left_action_items: [["menu", lambda x: app.navigation_draw()]]
-
-    MDRectangleFlatButton:
-        text: 'START'
-        pos_hint: {'center_x':.5,'center_y':0.5}
-        on_press: root.manager.current = 'drying'
-        text_color: 0, 0, 0, 1
-        md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-
-    MDRectangleFlatButton:
-        text: 'BACK'
-        pos_hint: {'center_x':.5,'center_y':0.3}
-        on_press: root.manager.current = 'select'
-        text_color: 0, 0, 0, 1
-        md_bg_color: .752,.514,.18,1
-        font_weight: "bold"
-
-
-
-
-
+        font_weight: "bold"     
+       
+<Done>:
+    MDDialog:
 
 """
--- Project/FoodDrier/mainApp.py
+++ Project/FoodDrier/mainApp.py
@@ -1,12 +1,28 @@
-from kivymd.app import MDApp
-from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from helpers import screen_helper
 from kivy.core.window import Window
-# from kivy.uix.boxlayout import BoxLayout
-# from kivymd.theming import ThemableBehavior
-# from kivymd.uix.list import MDList
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
+from helpers import screen_helper
+import sys
 
+# import pyrebase
+#
+# firebaseConfig = {
+#     "apiKey": "AIzaSyBnMYP_Qrp3n0hm7Mg3LQPhWpJ_XXuX6WE",
+#     "authDomain": "fooddrier.firebaseapp.com",
+#     "databaseURL": "https://fooddrier.firebaseio.com",
+#     "projectId": "fooddrier",
+#     "storageBucket": "fooddrier.appspot.com",
+#     "messagingSenderId": "140086005841",
+#     "appId": "1:140086005841:web:af467326aaa3bf761c2870",
+#     "measurementId": "G-ZKYHQPJBEW"}
+#
+# firebase = pyrebase.initialize_app(firebaseConfig)
+#
+# db = firebase.database()
 
 Window.size = (300, 500)
 
@@ -22,13 +38,24 @@ class HomeWithPowerOn(Screen):
 class SelectItemWindow(Screen):
     pass
 
+
 class DryingWindow(Screen):
     pass
+
 
 class Drying(Screen):
     pass
 
+
 class ResumeDrying(Screen):
+    pass
+
+
+class History(Screen):
+    pass
+
+
+class Done(Screen):
     pass
 
 
@@ -46,16 +73,125 @@ sm.add_widget(HomeWithPowerOn(name='power on'))
 sm.add_widget(SelectItemWindow(name='select'))
 sm.add_widget(DryingWindow(name='dry'))
 sm.add_widget(ResumeDrying(name='resume'))
-
-
+sm.add_widget(History(name='history'))
+sm.add_widget(Done(name='done'))
 
 
 class FoodDrier(MDApp):
+    dialog = None
+    id = None
 
     def build(self):
         screen = Builder.load_string(screen_helper)
-        self.theme_cls.primary_palette =  'BlueGray'
         return screen
+
+    data = {
+        'account-heart': '',
+        'send': '',
+        'settings': '',
+
+    }
+
+    def popup(self):
+        if not self.dialog:
+            self.dialog = MDDialog(text=" ! Please Restart The Application And Check Your Internet Connection",
+                                   buttons=[MDFlatButton(text='Ok', text_color=self.theme_cls.primary_color,
+
+                                                         )],
+                                   size_hint=(None, None),
+                                   # pos_hint={'y': 0.82},
+                                   size=(250, 0))
+
+        self.dialog.open()
+
+    # def popup2(self):
+    #     if not self.dialog:
+    #         self.dialog = MDDialog(text=" ",
+    #                                buttons=[MDFlatButton(text='Done', text_color=self.theme_cls.primary_color,
+    #                                                      on_press=self.start_stop(),
+    #
+    #                                                      )],
+    #                                size_hint=(None, None),
+    #                                # pos_hint={'y': 0.82},
+    #                                size=(250, 0))
+    #
+    #     self.dialog.open()
+
+    def close_app(self):
+
+        sys.exit('Connection')
+
+    def err_handling(self):
+        try:
+            self.power_on()
+
+        except:
+            self.popup()
+
+
+# def power_on(self):
+#     # db.child("food_drier").child("function").child("drying").update({"dry": 1})
+#     db.child("food_drier").child("status").update({"power_on": 1})
+#
+# # updating the data base after pressing a button
+# # thus for intergration of the android app and the raspberry pi
+#
+# # updating the power on value by pressing a power on button on the android app
+# def power_off(self):
+#     # db.child("food_drier").child("function").child("drying").update({"dry": 0})
+#     db.child("food_drier").child("status").update({"power_on": 0})
+#
+# # updating the dry value by pressing a dry button on the android app
+# # setting the drier ready for the drying process
+# def dry(self):
+#     db.child("food_drier").child("function").child("drying").update({"dry": 1})
+#
+# # updating the dry value by pressing a dry button on the android app
+# # removing the drier from the ready mode
+# def dry_off(self):
+#     db.child("food_drier").child("function").child("drying").update({"dry": 0})
+#
+# # updating the fruits value by pressing a fruit button on the android app
+# # telling the drier that the food we are trying to dry are fruits
+# # calling the fruit function in the pi
+#
+# def fruits(self):
+#     db.child("food_drier").child("status").update({"fruits": 1})
+#
+# # updating the vegies value by pressing a vegies button on the android app
+# # telling the drier that the food we are trying to dry are vegies
+# # calling the vegies function in the pi
+# def vegies(self):
+#     db.child("food_drier").child("status").update({"vegies": 1})
+#
+# # updating the meat value by pressing a meat button on the android app
+# # telling the drier that the food we are trying to dry are meat
+# # calling the meat function in the pi
+# def nyama(self):
+#     db.child("food_drier").child("status").update({"meat": 1})
+#
+# # updating the fish value by pressing a fish button on the android app
+# # telling the drier that the food we are trying to dry are fish
+# # calling the fish function in the pi
+# def fish(self):
+#     db.child("food_drier").child("status").update({"fish": 1})
+#
+# # on clicking the back button on the select food window, all the selected foods should be deactivated
+# # no function of a food should be called in the pi
+# def back_on_select(self):
+#     db.child("food_drier").child("status").update({"fruits": 0})
+#     db.child("food_drier").child("status").update({"vegies": 0})
+#     db.child("food_drier").child("status").update({"meat": 0})
+#     db.child("food_drier").child("status").update({"fish": 0})
+#
+# # start the drying after selecting the specified food and clicking the start button
+# def start_stop(self):
+#     db.child("food_drier").child("status").update({"start": 1})
+#
+# # stop the ongoing drying process
+# # resume the stopped drying process
+# def start_stop_stop(self):
+#     db.child("food_drier").child("status").update({"start": 0})
 
 
 FoodDrier().run()
